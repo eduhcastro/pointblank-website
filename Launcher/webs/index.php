9532d65fd0252b173ad5b1deecd1985040602285
skillerm <37242504+skillerm@users.noreply.github.com>
2020-11-06 19:26:03 -0300

    Add files via upload

---
+++ Launcher/webs/index.php
@@ -0,0 +1,68 @@
+﻿<?php
+ $rNdom = rand (1111, 9999);  ?>
+<!DOCTYPE html><html lang="zh-tw"><head><meta http-equiv="content-type" content="text/html;charset=utf-8"><!--  -->
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fatality News</title>
+    <meta name="description" content="Register！Account！NG Charge！Facebook">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="PB Website">
+    <meta property="og:description" content="PB Website">
+    <meta property="og:url" content="https://pb.tamgame.com/">
+    <meta property="og:image" content="http://image.nfinitygames.com/pbtr/v3launcheren/Content/HomePage/images/1200x628.jpg">
+    <meta property="og:site_name" content="PBTR">
+    <link rel="Shortcut Icon" type="image/x-icon" href="content/PointBlank.ico">
+	<!--[if lt IE 9]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.js"></script>
+	<![endif]-->
+    <link href="Content/HomePage/css/main.css?v=<?php echo $rNdom; ?>" rel="stylesheet">
+    <link href="Content/HomePage/css/launcher.css?v=<?php echo $rNdom; ?>" rel="stylesheet">
+	<style>
+	.contents_launcher {
+  overflow: hidden;
+  width: 445px;
+  height: 152px;
+  background: url("Content/images/launcher_bg.jpg?v=<?php echo $rNdom; ?>") no-repeat;
+}
+</style>
+</head>
+<body id="launcher">
+    <!--公告區塊-->
+<section class="contents_launcher">
+        <div class="launcher_wrap">
+            <a href="https://pb.tamgame.com/news/notice/list.do" 
+			target="_blank"><h3>Novidades Fatality</h3></a><h5><a href="https://pbfatality.com.br/" 
+			target="_blank"> Criar conta </a> | <a href="https://pbfatality.com.br/pages/cadastrar/"
+			target="_blank"> Discord </a>  <a href="https://discordapp.com/invite/FSPNr9e" 
+			target="_blank">  </a></a></h5>
+            <table id="NewsList">
+                <caption>Novidades</caption>
+                <colgroup>
+                    <col width="60px">
+                    <col width="275px">
+                    <col width="95px">
+                </colgroup>
+            <tbody><tr>                                  
+			<td><span class="icon">1</span></td>                                    
+			<td class="set_width text_left"><a href="https://pbfatality.com.br/" title="AUG A3 is $0.30 on August" target="_blank">1 Novo Launcher update!</a></td>                                    
+			<td>24/03/2020</td>                                
+			</tr><tr>                                   
+			<td><span class="icon">2</span></td>                                    
+			<td class="set_width text_left"><a href="https://pbfatality.com.br/" title="8 August Patch Notes" target="_blank">Bonus de 30% XP em DM.</a></td>                                  
+			<td>24/03/2020</td>                                </tr><tr>                                   
+			<td><span class="icon">3</span></td>                                    
+			<td class="set_width text_left"><a href="https://pbfatality.com.br/" title="Weekly Events" target="_blank">Novo Site Project Fatality.</a></td>                                  
+			<td>24/03/2020</td>                              
+			</tr><tr>                                   
+			<td><span class="icon">4</span></td>                                    
+			<td class="set_width text_left"><a href="https://pbfatality.com.br/" title="Arena-4 Registeration is Open" target="_blank">Update de armas na loja.</a></td>                                   
+			<td>24/03/2020</td>                                </tr></tbody></table>
+        </div>
+    </section>
+
+
+
+</body>
+</html>
